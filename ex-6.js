1048810a0f45a270b84996203a63d1b7ed1f6f41
doctor <fd5@MacBook-Air-2022.local>
2023-12-13 16:28:49 +0700

    ex7

--- ex-6.js
+++ ex-6.js
@@ -1,4 +1,4 @@
 let height = undefined;
-let result = height ?? "Height is not defined";
+let result = height ?? "Height is not defied";
 
 console.log(result); //  Result ควรจะได้ออกมาเป็น “Height is not defined”
